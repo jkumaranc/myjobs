4ab09da666ceb022e15c7b3c7f90a2497cdd411a
jkumaranc@gmail.com <jkumaranc@gmail.com@7c2fb4a1-335f-0480-f30e-5f65fd3ced0a>
2011-08-03 02:15:54 +0000

    Doing the major refactoring inorder to support other web framework

---
+++ trunk/myjobs-jsf2/src/main/java/org/jk/myjobs/managedbeans/ManagedUser.java
@@ -0,0 +1,162 @@
+/**
+ * Copyright [2011] [JK]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+package org.jk.myjobs.managedbeans;
+
+import org.jk.myjobs.RepositoryException;
+import org.jk.myjobs.dao.Repository;
+import org.jk.myjobs.domain.Application;
+import org.jk.myjobs.domain.User;
+import org.jk.myjobs.utils.inspectors.annotations.CompId;
+import org.jk.myjobs.utils.inspectors.annotations.EqualField;
+import org.jk.myjobs.utils.inspectors.annotations.Regex;
+import org.metawidget.inspector.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+import java.util.Collection;
+import java.util.List;
+
+import static org.jk.myjobs.utils.FacesUtils.handleError;
+
+/**
+ * User: JK
+ * Date: 28/07/11
+ * email: jkumaranc@yahoo.com
+ * User Delegated managed class
+ * Used for creation too as its more of an entity operation
+ */
+@ManagedBean(name = "mUser")
+@RequestScoped
+public class ManagedUser extends User {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ManagedUser.class);
+
+    @ManagedProperty(value = "#{repository}")
+    private Repository repository;
+    private transient String repeatPassword;
+
+    public String createUser() {
+        try {
+            repository.createUser(this);
+            LOGGER.info("New is created : " + getUserName());
+        } catch (RepositoryException e) {
+            LOGGER.error("Error when adding the user ", e);
+            handleError(FacesContext.getCurrentInstance(), "The user already exists");
+            return null;
+        }
+        return "/public/login.jsf";
+    }
+
+    @UiRequired
+    @UiLabel(value = "Full Name")
+    public String getFullName() {
+        return super.getFullName();
+    }
+
+    @UiRequired
+    @UiLabel(value = "Preferred User Name")
+    @UiComesAfter(value = "fullName")
+    public String getUserName() {
+        return super.getUserName();
+    }
+
+    @UiMasked
+    @UiRequired
+    @UiComesAfter(value = "userName")
+    @Regex(pattern = "((?=.*\\d)(?=.*\\w).{5,100})", message = "invalid.password")
+    @CompId(name = "password")
+    public String getPassword() {
+        return super.getPassword();
+    }
+
+    @UiMasked
+    @UiRequired
+    @UiComesAfter(value = "password")
+    @EqualField(field = "password", message = "password_missmatching")
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    @UiRequired
+    @UiComesAfter(value = "repeatPassword")
+    @Regex(pattern = ".+@.+\\.[a-z]+", message = "validator_email_message")
+    public String getEmail() {
+        return super.getEmail();
+    }
+
+    @UiLarge
+    @UiComesAfter(value = "email")
+    public String getAddress() {
+        return super.getAddress();
+    }
+
+
+    @UiHidden
+    public Repository getRepository() {
+        return repository;
+    }
+
+    @UiHidden
+    public String getUsername() {
+        return super.getUsername();
+    }
+
+    @UiHidden
+    public boolean isAccountNonExpired() {
+        return super.isAccountNonExpired();
+    }
+
+    @UiHidden
+    public boolean isAccountNonLocked() {
+        return super.isAccountNonLocked();
+    }
+
+    @UiHidden
+    public boolean isCredentialsNonExpired() {
+        return super.isCredentialsNonExpired();
+    }
+
+    @UiHidden
+    public boolean isEnabled() {
+        return super.isEnabled();
+    }
+
+    public void setRepository(Repository repository) {
+        this.repository = repository;
+    }
+
+    @UiHidden
+    public List<Application> getApplications() {
+        return super.getApplications();
+    }
+
+    @UiHidden
+    public boolean isHasApplications() {
+        return super.isHasApplications();
+    }
+
+    @UiHidden
+    public Collection<GrantedAuthority> getAuthorities() {
+        return super.getAuthorities();
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
+    }
+}
---
+++ trunk/myjobs-jsf2/src/main/java/org/jk/myjobs/validators/EqualityValidator.java
@@ -0,0 +1,354 @@
+package org.jk.myjobs.validators;
+
+import javax.el.ELException;
+import javax.el.ValueExpression;
+import javax.faces.FacesException;
+import javax.faces.application.Application;
+import javax.faces.component.EditableValueHolder;
+import javax.faces.component.PartialStateHolder;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.faces.render.Renderer;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.jk.myjobs.utils.FacesUtils.buildErrorFacesMessage;
+
+
+/**
+ * Validate two fields are equal
+ *
+ * @author pmuir
+ * @author Daniel Roth
+ *         <p/>
+ *         Updated from SEAM, so referred them!
+ */
+@FacesValidator(value = "org.jboss.seam.ui.validator.Equality")
+public class EqualityValidator implements Validator, PartialStateHolder {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EqualityValidator.class);
+
+    private enum ValidOperation {
+        EQUAL, NOT_EQUAL, GREATER, GREATER_OR_EQUAL, LESS, LESS_OR_EQUAL;
+    }
+
+    private String forId;
+    private String message;
+    private String messageId;
+    private ValidOperation operator = ValidOperation.EQUAL; // Default
+
+    public EqualityValidator() {
+        this.message = "Value does not equal that in '#0'";
+    }
+
+    public EqualityValidator(String forId) {
+        this();
+        setFor(forId);
+    }
+
+    public EqualityValidator(String forId, String message, String messageId, String operator) {
+        this(forId);
+        if (message != null) {
+            setMessage(message);
+        }
+        if (messageId != null) {
+            setMessageId(messageId);
+        }
+        if (operator != null && !"".equals(operator)) {
+            if (ValidOperation.valueOf(operator.toUpperCase()) != null)
+                setOperator(ValidOperation.valueOf(operator.toUpperCase()));
+            else
+                throw new IllegalStateException("Illegal operator. " + "Supported are: " + validOperatorsAsString());
+        }
+
+    }
+
+    private String validOperatorsAsString() {
+        StringBuffer buff = new StringBuffer();
+        for (ValidOperation op : ValidOperation.values()) {
+            buff.append(op.name()).append(" ");
+        }
+        return buff.toString();
+    }
+
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        String forId = getFor();
+        String forIdWithForm = "form:" + forId;
+        if (forId == null) {
+            throw new FacesException("Must specify a component to validate equality against");
+        }
+        UIComponent otherComponent = component.findComponent(forId);
+        if (otherComponent == null) {
+            otherComponent = component.findComponent(forIdWithForm);
+        }
+        Object other = new OtherComponent(context, otherComponent).getValue();
+        if (value == null && other == null) {
+            // Thats fine
+        } else if (value != null) {
+            switch (operator) {
+                case EQUAL:
+                    if (!value.equals(other)) {
+                        throw new ValidatorException(buildErrorFacesMessage(context, message));
+                    }
+                    break;
+                case NOT_EQUAL:
+                    if (value.equals(other)) {
+                        throw new ValidatorException(buildErrorFacesMessage(context, message));
+                    }
+                    break;
+                case GREATER:
+                    if (!(compare(value, other) > 0)) {
+                        throw new ValidatorException(buildErrorFacesMessage(context, message));
+                    }
+                    break;
+                case GREATER_OR_EQUAL:
+                    if (!(compare(value, other) >= 0)) {
+                        throw new ValidatorException(buildErrorFacesMessage(context, message));
+                    }
+                    break;
+                case LESS:
+                    if (!(compare(value, other) < 0)) {
+                        throw new ValidatorException(buildErrorFacesMessage(context, message));
+                    }
+                    break;
+                case LESS_OR_EQUAL:
+                    if (!(compare(value, other) <= 0)) {
+                        throw new ValidatorException(buildErrorFacesMessage(context, message));
+                    }
+                    break;
+            }
+        }
+    }
+
+    private int compare(Object value, Object other) throws IllegalArgumentException {
+        try {
+            Comparable c1 = (Comparable) value;
+            return c1.compareTo(other);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Values are not comparable", e);
+        }
+
+    }
+
+    public String getFor() {
+        return forId;
+    }
+
+    public void setFor(String forId) {
+        this.forId = forId;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    public boolean isTransient() {
+        return false;
+    }
+
+    //////////
+
+    // RESTORE & SAVE STATE
+    public Object saveState(FacesContext context) {
+        if (!initialStateMarked()) {
+            Object values[] = new Object[4];
+            values[0] = forId;
+            values[1] = message;
+            values[2] = messageId;
+            values[3] = operator.toString();
+            return values;
+        }
+        return null;
+    }
+
+    public void restoreState(FacesContext context, Object state) {
+        if (state != null) {
+            Object fields[] = (Object[]) state;
+            forId = (String) fields[0];
+            message = (String) fields[1];
+            messageId = (String) fields[2];
+            operator = ValidOperation.valueOf((String) fields[3]);
+        }
+    }
+
+    // MISC
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RegexValidator)) return false;
+
+        final EqualityValidator equalityValidator = (EqualityValidator) o;
+
+        if (forId != null ? !forId.equals(equalityValidator.forId) : equalityValidator.forId != null) return false;
+        if (message != null ? !message.equals(equalityValidator.message) : equalityValidator.message != null)
+            return false;
+
+        return true;
+    }
+
+    private boolean _initialStateMarked = false;
+
+    public void clearInitialState() {
+        _initialStateMarked = false;
+    }
+
+    public boolean initialStateMarked() {
+        return _initialStateMarked;
+    }
+
+    public void markInitialState() {
+        _initialStateMarked = true;
+    }
+
+
+    public void setTransient(boolean newTransientValue) {
+        // No-op
+    }
+
+    /**
+     * Simple data structure to hold info on the "other" component
+     *
+     * @author pmuir
+     */
+    private class OtherComponent {
+
+        private FacesContext context;
+        private UIComponent component;
+        private EditableValueHolder editableValueHolder;
+
+        private Renderer renderer;
+        private Converter converter;
+
+        public OtherComponent(FacesContext facesContext, UIComponent component) {
+            this.component = component;
+            this.context = facesContext;
+            if (!(component instanceof EditableValueHolder)) {
+                throw new IllegalStateException("forId must reference an EditableValueHolder (\"input\") component");
+            }
+            editableValueHolder = (EditableValueHolder) component;
+            initRenderer();
+            initConverter();
+        }
+
+        private void initRenderer() {
+            if (renderer == null) {
+                String rendererType = component.getRendererType();
+                if (rendererType != null) {
+                    renderer = context.getRenderKit().getRenderer(component.getFamily(), rendererType);
+                    if (null == renderer) {
+                        LOGGER.trace("Can't get Renderer for type " + rendererType);
+                    }
+                } else {
+                    if (LOGGER.isTraceEnabled()) {
+                        String id = component.getId();
+                        id = (null != id) ? id : component.getClass().getName();
+                        LOGGER.trace("No renderer-type for component " + id);
+                    }
+                }
+            }
+        }
+
+        private void initConverter() {
+            converter = editableValueHolder.getConverter();
+            if (converter != null) {
+                return;
+            }
+
+            ValueExpression valueExpression = component.getValueExpression("value");
+            if (valueExpression == null) {
+                return;
+            }
+
+            Class converterType;
+            try {
+                converterType = valueExpression.getType(context.getELContext());
+            } catch (ELException e) {
+                throw new FacesException(e);
+            }
+
+            // if converterType is null, String, or Object, assume
+            // no conversion is needed
+            if (converterType == null || converterType == String.class || converterType == Object.class) {
+                return;
+            }
+
+            // if getType returns a type for which we support a default
+            // conversion, acquire an appropriate converter instance.
+            try {
+                Application application = context.getApplication();
+                converter = application.createConverter(converterType);
+            } catch (Exception e) {
+                throw new FacesException(e);
+            }
+        }
+
+        private Object getConvertedValue(Object newSubmittedValue) throws ConverterException {
+
+            Object newValue;
+
+            if (renderer != null) {
+                newValue = renderer.getConvertedValue(context, component, newSubmittedValue);
+            } else if (newSubmittedValue instanceof String) {
+                // If there's no Renderer, and we've got a String, run it
+                // through
+                // the Converter (if any)
+                if (converter != null) {
+                    newValue = converter.getAsObject(context, component, (String) newSubmittedValue);
+                } else {
+                    newValue = newSubmittedValue;
+                }
+            } else {
+                newValue = newSubmittedValue;
+            }
+            return newValue;
+        }
+
+        public Object getValue() {
+            /**
+             * If conversion already is done, return value
+             */
+            if (editableValueHolder.isLocalValueSet()) {
+                return editableValueHolder.getValue();
+            }
+
+            /**
+             * Convert submittet value
+             */
+            Object submittedValue = editableValueHolder.getLocalValue();
+            if (submittedValue == null) {
+                return null;
+            }
+
+            Object newValue = null;
+
+            try {
+                newValue = getConvertedValue(submittedValue);
+            } catch (ConverterException ce) {
+                // Any errors will be attached by JSF
+                return null;
+            }
+
+            return newValue;
+        }
+
+    }
+
+    public ValidOperation getOperator() {
+        return operator;
+    }
+
+    public void setOperator(ValidOperation operator) {
+        this.operator = operator;
+    }
+
+
+}
